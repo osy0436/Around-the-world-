55ba1854873a849c2228c08535c539136987c3b6
osy0436 <103423645+osy0436@users.noreply.github.com>
2022-08-09 23:57:39 +0530

    Update autocomplete.js

--- autocomplete.js
+++ autocomplete.js
@@ -4,19 +4,19 @@
             const MIN_ADDRESS_LENGTH = 3;
             const DEBOUNCE_DELAY = 300;
 
-            // create container for input element
+            
             const inputContainerElement = document.createElement("div");
             inputContainerElement.setAttribute("class", "input-container");
             containerElement.appendChild(inputContainerElement);
 
-            // create input element
+           
             const inputElement = document.createElement("input");
             inputElement.setAttribute("type", "text");
             inputElement.setAttribute("id", "textfield1");
             inputElement.setAttribute("placeholder", options.placeholder);
             inputContainerElement.appendChild(inputElement);
 
-            // add input field clear button
+           
             const clearButton = document.createElement("div");
             clearButton.classList.add("clear-button");
             addIcon(clearButton);
@@ -29,29 +29,29 @@
             });
             inputContainerElement.appendChild(clearButton);
 
-            /* We will call the API with a timeout to prevent unneccessary API activity.*/
+           
             let currentTimeout;
 
-            /* Save the current request promise reject function. To be able to cancel the promise when a new request comes */
+            
             let currentPromiseReject;
 
-            /* Focused item in the autocomplete list. This variable is used to navigate with buttons */
+           
             let focusedItemIndex;
 
-            /* Process a user input: */
+            
             inputElement.addEventListener("input", function (e) {
                 const currentValue = this.value;
 
-                /* Close any already open dropdown list */
+              
                 closeDropDownList();
 
 
-                // Cancel previous timeout
+                
                 if (currentTimeout) {
                     clearTimeout(currentTimeout);
                 }
 
-                // Cancel previous request promise
+               
                 if (currentPromiseReject) {
                     currentPromiseReject({
                         canceled: true
@@ -62,24 +62,23 @@
                     clearButton.classList.remove("visible");
                 }
 
-                // Show clearButton when there is a text
+              
                 clearButton.classList.add("visible");
 
-                // Skip empty or short address strings
+             
                 if (!currentValue || currentValue.length < MIN_ADDRESS_LENGTH) {
                     return false;
                 }
 
-                /* Call the Address Autocomplete API with a delay */
+           
                 currentTimeout = setTimeout(() => {
                     currentTimeout = null;
 
-                    /* Create a new promise and send geocoding request */
+                  
                     const promise = new Promise((resolve, reject) => {
                         currentPromiseReject = reject;
 
-                        // The API Key provided is restricted to JSFiddle website
-                        // Get your own API Key on https://myprojects.geoapify.com
+                      
                         const apiKey = "b5ccc701212d45658ee3cc25830021f7";
 
                         var url = `https://api.geoapify.com/v1/geocode/autocomplete?text=${encodeURIComponent(currentValue)}&format=json&limit=5&apiKey=${apiKey}`;
@@ -88,7 +87,7 @@
                             .then(response => {
                                 currentPromiseReject = null;
 
-                                // check if the call was successful
+                                
                                 if (response.ok) {
                                     response.json().then(data => resolve(data));
                                 } else {
@@ -98,27 +97,27 @@
                     });
 
                     promise.then((data) => {
-                        // here we get address suggestions
+                       
                         currentItems = data.results;
 
-                        /*create a DIV element that will contain the items (values):*/
+                       
                         const autocompleteItemsElement = document.createElement("div");
                         autocompleteItemsElement.setAttribute("class", "autocomplete-items");
                         inputContainerElement.appendChild(autocompleteItemsElement);
 
-                        /* For each item in the results */
+                      
                         data.results.forEach((result, index) => {
-                            /* Create a DIV element for each element: */
+                          
                             const itemElement = document.createElement("div");
-                            /* Set formatted address as item value */
+                           
                             itemElement.innerHTML = result.formatted;
                             autocompleteItemsElement.appendChild(itemElement);
 
-                            /* Set the value for the autocomplete text field and notify: */
+                          
                             itemElement.addEventListener("click", function (e) {
                                 inputElement.value = currentItems[index].formatted;
                                 callback(currentItems[index]);
-                                /* Close the list of autocompleted values: */
+                             
                                 closeDropDownList();
                             });
                         });
@@ -131,26 +130,26 @@
                 }, DEBOUNCE_DELAY);
             });
 
-            /* Add support for keyboard navigation */
+         
             inputElement.addEventListener("keydown", function (e) {
                 var autocompleteItemsElement = containerElement.querySelector(".autocomplete-items");
                 if (autocompleteItemsElement) {
                     var itemElements = autocompleteItemsElement.getElementsByTagName("div");
                     if (e.keyCode == 40) {
                         e.preventDefault();
-                        /*If the arrow DOWN key is pressed, increase the focusedItemIndex variable:*/
+                       
                         focusedItemIndex = focusedItemIndex !== itemElements.length - 1 ? focusedItemIndex + 1 : 0;
-                        /*and and make the current item more visible:*/
+                      
                         setActive(itemElements, focusedItemIndex);
                     } else if (e.keyCode == 38) {
                         e.preventDefault();
 
-                        /*If the arrow UP key is pressed, decrease the focusedItemIndex variable:*/
+                      
                         focusedItemIndex = focusedItemIndex !== 0 ? focusedItemIndex - 1 : focusedItemIndex = (itemElements.length - 1);
-                        /*and and make the current item more visible:*/
+                        
                         setActive(itemElements, focusedItemIndex);
                     } else if (e.keyCode == 13) {
-                        /* If the ENTER key is pressed and value as selected, close the list*/
+                        
                         e.preventDefault();
                         if (focusedItemIndex > -1) {
                             closeDropDownList();
@@ -158,7 +157,7 @@
                     }
                 } else {
                     if (e.keyCode == 40) {
-                        /* Open dropdown list again */
+                       
                         var event = document.createEvent('Event');
                         event.initEvent('input', true, true);
                         inputElement.dispatchEvent(event);
@@ -173,10 +172,10 @@
                     items[i].classList.remove("autocomplete-active");
                 }
 
-                /* Add class "autocomplete-active" to the active element*/
+              
                 items[index].classList.add("autocomplete-active");
 
-                // Change input value and notify
+              
                 inputElement.value = currentItems[index].formatted;
                 callback(currentItems[index]);
             }
@@ -202,13 +201,12 @@
                 buttonElement.appendChild(svgElement);
             }
 
-            /* Close the autocomplete dropdown when the document is clicked. 
-              Skip, when a user clicks on the input field */
+           
             document.addEventListener("click", function (e) {
                 if (e.target !== inputElement) {
                     closeDropDownList();
                 } else if (!containerElement.querySelector(".autocomplete-items")) {
-                    // open dropdown list again
+                  
                     var event = document.createEvent('Event');
                     event.initEvent('input', true, true);
                     inputElement.dispatchEvent(event);
